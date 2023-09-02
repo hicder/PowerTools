e85998092a2a89b84227da8632ea1c39a8c0c055
Hieu Pham <dpham810@gmail.com>
2023-09-02 09:24:53 -0700

    Fix install script

--- backend/build_and_install.sh
+++ backend/build_and_install.sh
@@ -5,5 +5,5 @@ cargo build --release
 
 echo "Install the backend binary..."
 sudo systemctl stop plugin_loader
-sudo cp --preserve=mode ./target/release/powertools ../bin/backend
+sudo cp --preserve=mode ./target/release/powertools $HOME/homebrew/plugins/PowerTools/bin/backend
 sudo systemctl start plugin_loader
